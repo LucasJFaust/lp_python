3e9c39cd18fe69bed89183388c09d37c22575510
LucasJFaust <lucasjfm.eng@gmail.com>
2024-11-09 16:47:31 -0300

    exemplo 3.4-1

---
+++ exemplo_3-4_pg51.py
@@ -0,0 +1,12 @@
+"""
+Contrua um algoritimo que calcule a média aritimética entre 4 notas
+bimestrai quaisquer fornecidas por um aluno.
+"""
+
+n1 = float(input("Digite a primeira nota: "))
+n2 = float(input("Digite a primeira segunda: "))
+n3 = float(input("Digite a primeira terceira: "))
+n4 = float(input("Digite a primeira quarta: "))
+media_aritimetica = (n1 + n2 + n3 + n4) / 4
+
+print(f"Sua média foi {media_aritimetica}")
